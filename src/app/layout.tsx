801a5e167826645eb065014b35a00c6eb84592c3
gabrielssouza23 <94071390+gabrielssouza23@users.noreply.github.com>
2023-06-12 18:37:30 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.scss'
-import { Poppins } from 'next/font/google'
+import './globals.scss';
+import { Poppins } from 'next/font/google';
 
-const inter = Poppins({ subsets: ['latin',], weight: ['300', '400', '500', '600'] })
+const inter = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600'] });
 
 export const metadata = {
-  title: 'Gabriel de Souza',
+  title: 'Portfolio',
   description: 'Gabriel de Souza - Software Developer',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>  
+        <link rel="icon" href="/code.png" type="image/png"/>
+
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
